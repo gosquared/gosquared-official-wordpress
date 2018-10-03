c9dc471328b4bab2ae1e1618e5169e4d783760e1
russellvaughan <russellvaughan@gmail.com>
2018-10-03 12:33:28 +0100

    capture email regardless of label name and remove console.logs

--- includes/gosquared-official-gravity-forms-integration.php
+++ includes/gosquared-official-gravity-forms-integration.php
@@ -6,15 +6,12 @@ class GoSquaredGFIntegration {
 
   public function __construct($project_token) {
     $this->project_token = $project_token;
-    echo "<script>console.log('$this->project_token')</script>";
     add_action( 'gform_after_submission',  array($this, 'send'), 10, 2 );
   }
 
   public function send($entry, $form) {
     $standard_props = array(
         'Email' => 'email',
-        'Email Address' => 'email',
-        'Your Email Address' => 'email',
         'Name'=> 'name',
         'First Name' => 'first_name',
         'Last Name' => 'last_name',
@@ -26,7 +23,7 @@ class GoSquaredGFIntegration {
         'Job Title' => 'company_position',
     );
 
-    $valid_types = array(text, website, phone, number, date, time, name, address, email);
+    $valid_types = array(text, website, phone, number, date, time, name, address, email, username);
 
     $fields = $form['fields'];
     ?>
@@ -41,9 +38,6 @@ class GoSquaredGFIntegration {
            if (is_array($f["inputs"])) {
             foreach($f["inputs"] as $input) {
               if (in_array($input['type'], $valid_types)) {
-                ?>
-                console.log('<?php echo json_encode($f) ?>')
-                <?php
               $object_key = $standard_props[$input['label']];
               $object_value = $entry[$input['id']];
               if(isset( $standard_props[$input['label']] )){
@@ -57,15 +51,17 @@ class GoSquaredGFIntegration {
                     ?>
                     properties.custom['<?php echo $input['label'] ?>'] = '<?php echo $object_value ?>';
                     <?php
+                    if ($f['type']==='email'){
+                        ?>
+                        properties['email'] = '<?php echo $object_value ?>';
+                        <?php
+                    }
                   }
                 }
               }
             }
           }
           if (in_array($f['type'], $valid_types)) {
-          ?>
-          console.log('<?php echo json_encode($f) ?>')
-          <?php
           $object_key = $standard_props[$f['label']];
           $object_value = $entry[$f['id']];
             if(isset( $standard_props[$f['label']] )){
@@ -79,6 +75,11 @@ class GoSquaredGFIntegration {
                   ?>
                   properties.custom['<?php echo $f['label'] ?>'] = '<?php echo $object_value ?>';
                   <?php
+                  if ($f['type']==='email'){
+                      ?>
+                      properties['email'] = '<?php echo $object_value ?>';
+                      <?php
+                  }
               }
             }
           }
@@ -89,10 +90,8 @@ class GoSquaredGFIntegration {
          arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
          d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
          insertBefore(d,q)}(window,document,'script','_gs');
-
         _gs('<?php echo $this->project_token; ?>');
         }
-
         _gs('identify', properties);
   </script>
 <?php }
