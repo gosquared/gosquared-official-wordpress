4a025d0707733ada2f2d04502cc4b5844be0f1cb
russellvaughan <russellvaughan@gmail.com>
2018-10-09 15:35:36 +0100

    fix function

---
+++ tests/GoSquaredOptionsPageTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class GoSquaredOptionsPageTestClass extends \WP_Mock\Tools\TestCase {
+
+	public function setUp() {
+		\WP_Mock::setUp();
+	}
+
+	public function tearDown() {
+		\WP_Mock::tearDown();
+	}
+}
